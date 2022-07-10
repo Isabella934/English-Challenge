b713a1b9e80b5c9620fdb0edb51e90b1893d0890
Isabella934 <isabella.lessa27@gmail.com>
2022-07-10 20:58:29 -0300

    english challenge

---
+++ script.js
@@ -0,0 +1,28 @@
+let comedy = [
+    {
+        title: "Just Go with It",
+        director: "Dennis Dugan",
+        year: 2011
+    }
+];
+
+if(title = "Just Go with It"){
+    console.log("It's my favorite movie");
+}else{
+    console.log("I don't like this one");
+}
+
+let action = [
+    {
+        title: "Abduction",
+        director: "Damien Power",
+        year: 2011
+    }
+];
+let fantasy = [
+    {
+        title: "Captain Marvel",
+        director: "Anna Boden",
+        year: 2019
+    }
+];
